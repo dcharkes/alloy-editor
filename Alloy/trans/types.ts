61f6bb17fe333c995bbe16e9d5903d2ae80f69cc
Daco Harkes <dc.harkes@gmail.com>
2015-01-29 17:58:56 +0100

    TS spec: union and relation types NaBL spec: fields

--- Alloy/trans/types.ts
+++ Alloy/trans/types.ts
@@ -18,3 +18,20 @@ type rules // subtype
 	SigDecl(a, [s], Some(Extends(super)), c, d) :-
   where super : super-ty
     and store s <sub: super-ty
+
+type rules // union types
+
+	RefUnion(a, b)
++	Union(a, b): ty
+	where a : a-ty
+	  and b : b-ty
+	  and Union([a-ty, b-ty]) => ty
+
+type rules // relation types
+
+	Arrow(a, a-mul, b-mul, b): rel-ty
+	where a : a-ty
+	  and b : b-ty
+	  and Rel([a-ty, b-ty]) => rel-ty
+
+type rules
