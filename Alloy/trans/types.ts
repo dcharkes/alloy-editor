881acb633633d3c0ebe4319a027604c112e3df9b
Daco Harkes <dc.harkes@gmail.com>
2015-01-30 15:38:49 +0100

    TS spec: check that predicate body is Boolean + test

--- Alloy/trans/types.ts
+++ Alloy/trans/types.ts
@@ -13,6 +13,27 @@ type rules // references
 	FieldRef(e, f): f-ty
 	where definition of f : f-ty
 
+type rules // expressions
+	
+	Int(n): Int()
+
+	Card(e): Int()
+	
+	Lt (e1, neg, e2)
++	Gt (e1, neg, e2)
++	Lte(e1, neg, e2)
++	Gte(e1, neg, e2): Boolean()
+	where	e1 : e1-ty
+	  and e2 : e2-ty
+	  and e1-ty => Int() else error ["Expected ", Int(), " got ", e1-ty] on e1
+	  and e2-ty => Int() else error ["Expected ", Int(), " got ", e2-ty] on e2
+
+type rules // declaration expressions should be booleans
+
+	PredExpr(e):-
+	where e : e-ty
+	  and e-ty => Boolean() else error ["Expected ", Boolean(), " got ", e-ty] on e
+
 relations // subtype
 
 	define transitive <sub:
